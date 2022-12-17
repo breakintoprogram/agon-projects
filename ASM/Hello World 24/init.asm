0d8485b9709e20e86b79ccb3a67d1ce329b3d326
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2022-12-17 11:22:16 +0000

    Added parameter processing

--- ASM/Hello World 24/init.asm
+++ ASM/Hello World 24/init.asm
@@ -2,36 +2,133 @@
 ; Title:	Hello World - Initialisation Code
 ; Author:	Dean Belfield
 ; Created:	06/11/2022
-; Last Updated:	06/11/2022
+; Last Updated:	17/12/2022
 ;
 ; Modinfo:
+; 17/12/2022:	Added parameter processing
 
 			SEGMENT CODE
 			
-			XREF	INIT_UART
-			XREF	INIT_IM2
-			XREF	GPIOB_SETMODE
-			XREF	MAIN
+			XREF	_main
 		
 			.ASSUME	ADL = 1
 				
 			INCLUDE	"equs.inc"
 			
+argv_ptrs_max:		EQU	16			; Maximum number of arguments allowed in argv
+			
 ;
 ; Start in ADL mode
 ;
-			JP		_START		; Jump to start			
+			JP	_start			; Jump to start			
 ;
 ; The header stuff is from byte 64 onwards
 ;
-			ALIGN		64
+
+_exec_name:		DB	"HELLO.BIN", 0		; The executable name, only used in argv
+
+			ALIGN	64
 			
-			DB		"MOS"		; Flag for MOS - to confirm this is a valid MOS command
-			DB		00h		; MOS header version 0
-			DB		01h		; Flag for run mode (0: Z80, 1: ADL)
+			DB	"MOS"			; Flag for MOS - to confirm this is a valid MOS command
+			DB	00h			; MOS header version 0
+			DB	01h			; Flag for run mode (0: Z80, 1: ADL)
 ;
 ; And the code follows on immediately after the header
 ;
-_START:			EI				; Enable the MOS interrupts
-			JP	MAIN			; Start user code
+_start:			PUSH	AF			; Preserve the registers
+			PUSH	BC
+			PUSH	DE
+			PUSH	IX
+			PUSH	IY
 			
+			LD	IX, argv_ptrs		; The argv array pointer address
+			PUSH	IX
+			CALL	_parse_params		; Parse the parameters
+			POP	IX			; IX: argv	
+			LD	B, 0			;  C: argc
+			CALL	_main			; Start user code
+			
+			POP	IY			; Restore registers
+			POP	IX
+			POP	DE
+			POP	BC
+			POP	AF
+			RET
+	
+; Parse the parameter string into a C array
+; Parameters
+; - HL: Address of parameter string
+; - IX: Address for array pointer storage
+; Returns:
+; -  C: Number of parameters parsed
+;
+_parse_params:		LD	BC, _exec_name
+			LD	(IX+0), BC		; ARGV[0] = the executable name
+			INC	IX
+			INC	IX
+			INC	IX
+			CALL	_skip_spaces		; Skip HL past any leading spaces
+;
+			LD	BC, 1			; C: ARGC = 1 - also clears out top 16 bits of BCU
+			LD	B, argv_ptrs_max - 1	; B: Maximum number of argv_ptrs
+;
+_parse_params_1:	
+			PUSH	BC			; Stack ARGC	
+			PUSH	HL			; Stack start address of token
+			CALL	_get_token		; Get the next token
+			LD	A, C			; A: Length of the token in characters
+			POP	DE			; Start address of token (was in HL)
+			POP	BC			; ARGC
+			OR	A			; Check for A=0 (no token found) OR at end of string
+			RET	Z
+;
+			LD	(IX+0), DE		; Store the pointer to the token
+			PUSH	HL			; DE=HL
+			POP	DE
+			CALL	_skip_spaces		; And skip HL past any spaces onto the next character
+			XOR	A
+			LD	(DE), A			; Zero-terminate the token
+			INC	IX
+			INC	IX
+			INC	IX			; Advance to next pointer position
+			INC	C			; Increment ARGC
+			LD	A, C			; Check for C >= A
+			CP	B
+			JR	C, _parse_params_1	; And loop
+			RET
+
+; Get the next token
+; Parameters:
+; - HL: Address of parameter string
+; Returns:
+; - HL: Address of first character after token
+; -  C: Length of token (in characters)
+;
+_get_token:		LD	C, 0			; Initialise length
+$$:			LD	A, (HL)			; Get the character from the parameter string
+			OR	A			; Exit if 0 (end of parameter string in MOS)
+			RET 	Z
+			CP	13			; Exit if CR (end of parameter string in BBC BASIC)
+			RET	Z
+			CP	' '			; Exit if space (end of token)
+			RET	Z
+			INC	HL			; Advance to next character
+			INC 	C			; Increment length
+			JR	$B
+	
+; Skip spaces in the parameter string
+; Parameters:
+; - HL: Address of parameter string
+; Returns:
+; - HL: Address of next none-space character
+;    F: Z if at end of string, otherwise NZ if there are more tokens to be parsed
+;
+_skip_spaces:		LD	A, (HL)			; Get the character from the parameter string	
+			CP	' '			; Exit if not space
+			RET	NZ
+			INC	HL			; Advance to next character
+			JR	_skip_spaces		; Increment length
+
+; Storage for the argv array pointers
+;
+argv_ptrs:		BLKP	argv_ptrs_max, 0			
--- ASM/Hello World 24/main.asm
+++ ASM/Hello World 24/main.asm
@@ -2,9 +2,10 @@
 ; Title:	Hello World - Main
 ; Author:	Dean Belfield
 ; Created:	06/11/2022
-; Last Updated:	06/11/2022
+; Last Updated:	17/12/2022
 ;
 ; Modinfo:
+; 17/12/2022:	Added parameter processing
 
 			.ASSUME	ADL = 1				
 
@@ -12,12 +13,36 @@
 
 			SEGMENT CODE
 			
-			XDEF	MAIN
+			XDEF	_main
 
 ; The main routine
+; IX: argv - pointer to array of parameters
+;  C: argc - number of parameters
+; Returns:
+; HL: Error code, or 0 if OK
 ;
-MAIN:			LD	HL, HELLO_WORLD
+_main:			LD	HL, s_HELLO_WORLD	; Print "Hello World"
 			CALL	PRSTR
+			LD	HL, s_ARGUMENTS		; Print "Arguments:"
+			CALL	PRSTR
+			
+			LD	B, C			; B: # of arguments
+			
+; Loop round printing the arguments
+;
+$$:			LD	HL, s_ARGV		; Print " - argv: "
+			CALL	PRSTR
+			LD	HL, (IX+0)		; Get the argument string address from the array
+			CALL	PRSTR			; And print it
+			INC	IX			; Increment to the next index in the array
+			INC	IX
+			INC	IX
+			LD	HL, s_CRLF		; Print a carriage return
+			CALL	PRSTR			
+			DJNZ	$B			; And loop
+			
+; Return with error code 0
+;
 			LD	HL, 0
 			RET
 	
@@ -32,7 +57,10 @@ PRSTR:			LD	A,(HL)
 
 ; Sample text
 ;
-HELLO_WORLD:		DB 	"Hello World\n\r", 0
+s_HELLO_WORLD:		DB 	"Hello World\n\r", 0
+s_ARGUMENTS:		DB	"Arguments:\n\r", 0
+s_ARGV:			DB	" - argv: ", 0
+s_CRLF:			DB	"\n\r", 0
 	
 ; RAM
 ; 
